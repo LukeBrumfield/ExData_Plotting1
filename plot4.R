248b44c17f2ed9912ae36dd17cf296f8545cc961
Luke Brumfield <LukeBrumfield@github.com>
2014-12-07 13:07:34 -0500

    Updated Plot4

--- plot4.R
+++ plot4.R
@@ -4,27 +4,30 @@ data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 data <- subset(data, subset = data$Date == dates[1] | data$Date == dates[2])
 
 
-par(mfrow = c(2,2), mar = c(4,4,4,1))
+
 days <- c("Thu", "Fri", "Sat")
 
 png("plot4.png")
 
-par(mfrow=c(2,2))
+par(mfrow = c(2,2), mar = c(4,4,4,1))
 
-plot(as.numeric(as.character(data$Global_active_power)), ylab = "Global Active Power (kilowats)")
-plot(as.numeric(as.character(data$Voltage)), type="1", ylab = "Voltage")
+plot(as.numeric(as.character(data$Global_active_power)), type = "l", ylab = "Global Active Power (kilowats)", xaxt = "n", xlab = "datetime")
+axis(side = 1, at = c(0, 1440, 2880), labels = days)
+plot(as.numeric(as.character(data$Voltage)), type="l", ylab = "Voltage", xaxt = "n", xlab = "")
+axis(side = 1, at = c(0, 1440, 2880), labels = days)
 
 
 x <- as.numeric(as.character(data$Sub_metering_1))
 y <- as.numeric(as.character(data$Sub_metering_2))
 z <- as.numeric(as.character(data$Sub_metering_3))
 
-plot(x, col = "black", type = "l", xaxt = "n")
+plot(x, col = "black", type = "l", xaxt = "n", xlab = "", ylab="Energy SubMetering")
 lines(y, col = "red", type = "l")
 lines(z, col = "blue", type = "l")
 axis(side = 1, at = c(0, 1440, 2880), labels = days)
 legend("topright", colnames(data)[7:9], lty=c(1,1), col=c("black","red","blue"))
 
-plot(as.numeric(as.character(data$Global_reactive_power)), ylab="Global Reactive Power")
+plot(as.numeric(as.character(data$Global_reactive_power)), type= "l", ylab="Global Reactive Power", xaxt = "n", xlab = "datetime")
+axis(side = 1, at = c(0, 1440, 2880), labels = days)
 
 dev.off()
